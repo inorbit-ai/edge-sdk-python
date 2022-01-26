81236fa9bcb76b5518cf6d64edc1feed56cabd8b
Leandro Pineda <leandro@inorbit.ai>
2022-01-26 15:55:59 -0300

    Removing dumb test

--- inorbit_edge/tests/test_robot_session.py
+++ inorbit_edge/tests/test_robot_session.py
@@ -1,23 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pytest
 from inorbit_edge.robot import RobotSession
 from inorbit_edge.robot import INORBIT_CLOUD_SDK_ROBOT_CONFIG_URL
-
-
-@pytest.mark.parametrize(
-    "robot_id, robot_name, app_key",
-    [
-        # (robot_id, robot_name, app_key)
-        ("abc123", "R2D2", "s3cr3t")
-    ],
-)
-def test_foo(robot_id, robot_name, app_key):
-    robot_session = RobotSession(robot_id, robot_name, app_key)
-    assert robot_session.robot_id == robot_id
-    assert robot_session.robot_name == robot_name
-    assert robot_session.app_key == app_key
 
 
 def test_robot_session_init(monkeypatch):
