6c1e83aa9e14bcac064230610f676846634f3867
Leandro Pineda <leandro@inorbit.ai>
2022-02-03 12:37:37 -0300

    Fix API key usage

--- inorbit_edge/tests/test_robot_session.py
+++ inorbit_edge/tests/test_robot_session.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from unittest.mock import MagicMock
 from inorbit_edge.robot import RobotSession
 from inorbit_edge.robot import INORBIT_CLOUD_SDK_ROBOT_CONFIG_URL
+from inorbit_edge.tests.test_fetch_robot_config import ROBOT_CONFIG_MOCK_RESPONSE
+from inorbit_edge import get_module_version
+import requests_mock
 
 
 def test_robot_session_init(monkeypatch):
@@ -38,4 +42,33 @@ def test_robot_session_init(monkeypatch):
                 robot_session.client._transport == "websockets",
                 robot_session.http_proxy == "http://foo_bar.com:1234",
             ]
+        )
+
+
+def test_robot_session_connect(monkeypatch):
+    robot_session = RobotSession(
+        robot_id="id_123", robot_name="name_123", api_key="apikey_123"
+    )
+
+    with requests_mock.Mocker() as mock:
+        mock.post(INORBIT_CLOUD_SDK_ROBOT_CONFIG_URL, json=ROBOT_CONFIG_MOCK_RESPONSE)
+        # mock mqtt client
+        monkeypatch.setattr(robot_session, "client", MagicMock())
+        # mock publish and is_published so it always returns True
+        is_published_mock = MagicMock()
+        is_published_mock.is_published.return_value = True
+        publish_mock = MagicMock(return_value=is_published_mock)
+        monkeypatch.setattr(robot_session, "publish", publish_mock)
+        # connect robot_session so it populates properties with API response data
+        robot_session.connect()
+        # manually execute on_connect callback to send robot status
+        robot_session.send_robot_status("1")
+        assert robot_session.api_key == "apikey_123"
+        assert robot_session.robot_api_key == "robot_apikey_123"
+        # check publish state was called with the correct API key
+        publish_mock.assert_called_with(
+            "r/id_123/state",
+            "1|robot_apikey_123|{}.edgesdk_py|name_123".format(get_module_version()),
+            qos=1,
+            retain=True,
         )
