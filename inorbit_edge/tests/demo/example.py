f308e2c9ed73a221d50cfb296bed39d167349a24
Leandro Pineda <leandro@inorbit.ai>
2022-02-18 12:43:00 -0300

    Revert logging to INFO

--- inorbit_edge/tests/demo/example.py
+++ inorbit_edge/tests/demo/example.py
@@ -13,7 +13,7 @@ from math import inf
 from inorbit_edge.robot import RobotSessionFactory, RobotSessionPool
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[logging.StreamHandler()],
 )
