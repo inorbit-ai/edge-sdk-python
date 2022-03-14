2502bbef2f052fd7cc8e43671d6eebbc53b6cf58
Mike Bird <mike@arrendy.com>
2022-03-14 16:38:18 -0400

    Bump version: 0.5.1 → 0.6.0

--- setup.py
+++ setup.py
@@ -85,6 +85,6 @@ setup(
     url="https://github.com/inorbit-ai/edge-sdk-python",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.5.1",
+    version="0.6.0",
     zip_safe=False,
 )
