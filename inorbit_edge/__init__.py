8dc3a228ab18c6a31ab2e53746bb8eb21eab3ee1
Leandro Pineda <leandro@inorbit.ai>
2022-04-01 15:09:24 -0300

    Bump version: 0.6.0 → 0.7.0

--- inorbit_edge/__init__.py
+++ inorbit_edge/__init__.py
@@ -6,7 +6,7 @@ __author__ = "InOrbit"
 __email__ = "support@inorbit.ai"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.6.0"
+__version__ = "0.7.0"
 
 
 def get_module_version():
