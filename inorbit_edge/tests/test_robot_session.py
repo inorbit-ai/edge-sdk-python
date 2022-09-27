62ade8b83a06cd8ae8201f3a894b24dd3fba9111
Leandro Pineda <leandro@inorbit.ai>
2022-09-27 10:58:29 -0300

    Fix UT

--- inorbit_edge/tests/test_robot_session.py
+++ inorbit_edge/tests/test_robot_session.py
@@ -79,15 +79,15 @@ def test_robot_session_connect(monkeypatch):
 
 
 def test_robot_session_custom_command_callback(monkeypatch):
-    def my_custom_command_handler(robot_session, msg):
+    def my_command_handler(robot_id, command_name, args, options):
         pass
 
     robot_session = RobotSession(
         robot_id="id_123",
         robot_name="name_123",
         api_key="apikey_123",
-        custom_command_callback=my_custom_command_handler,
     )
+    robot_session.register_command_callback(my_command_handler)
 
     with requests_mock.Mocker() as mock:
         mock.post(INORBIT_CLOUD_SDK_ROBOT_CONFIG_URL, json=ROBOT_CONFIG_MOCK_RESPONSE)
@@ -106,53 +106,8 @@ def test_robot_session_custom_command_callback(monkeypatch):
         # callback gets registered
         robot_session._on_connect(..., ..., ..., 0)
 
-        assert robot_session.custom_command_callback == my_custom_command_handler
-        mqtt_client_mock.subscribe.assert_called_with(topic="r/id_123/custom_command")
-
-
-@pytest.mark.parametrize(
-    "msg_payload, log_string",
-    [
-        (b"\x8f\xac\x93\x87 \x8fG\x95v", "Failed to decode message, ignoring."),
-        (b'{"msg":"foobar"}}}}}}}', "Failed to parse JSON message, ignoring."),
-    ],
-)
-def test_robot_session_on_message_callback(caplog, msg_payload, log_string):
-    def my_custom_command_handler(robot_session, msg):
-        pass
-
-    with caplog.at_level(logging.ERROR, logger=RobotSession.__name__):
-        robot_session = RobotSession(
-            robot_id="id_123",
-            robot_name="name_123",
-            api_key="apikey_123",
-            custom_command_callback=my_custom_command_handler,
-        )
-
-        mqtt_message = MQTTMessage(topic=b"r/id_123/custom_command")
-        mqtt_message.payload = msg_payload
-
-        robot_session._on_message(..., ..., mqtt_message)
-        assert log_string in caplog.text
-
-
-def test_robot_session_on_unsupported_topic(caplog):
-    def my_custom_command_handler(robot_session, msg):
-        pass
-
-    with caplog.at_level(logging.WARN, logger=RobotSession.__name__):
-        robot_session = RobotSession(
-            robot_id="id_123",
-            robot_name="name_123",
-            api_key="apikey_123",
-            custom_command_callback=my_custom_command_handler,
-        )
-
-        mqtt_message = MQTTMessage(topic=b"r/foobar_789/random_topic")
-
-        robot_session._on_message(..., ..., mqtt_message)
-        assert "Ignoring message from unsupported topic" in caplog.text
-
+        assert my_command_handler in robot_session.command_callbacks
+        mqtt_client_mock.subscribe.assert_called_with(topic="r/id_123/custom_command/script/command")
 
 def test_method_throttling():
     robot_session = RobotSession(
