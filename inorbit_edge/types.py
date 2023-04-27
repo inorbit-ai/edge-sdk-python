cdc1423a8ce5c208187ad68c5a08745e0efa8f4c
Miguel Angel Garcia <miguel.garcia@gmail.com>
2023-04-27 10:53:58 -0300

    missing modules

---
+++ inorbit_edge/types.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+@dataclass
+class Pose:
+    frame_id: str
+    x: float
+    y: float
+    theta: float
+
+@dataclass
+class SpatialTolerance:
+    positionMeters: float
+    angularRadians: float
