fffce5f44bab021fb4eee84e5b0369ce96dbdf21
Miguel Angel Garcia <miguel.garcia@gmail.com>
2023-05-02 13:34:48 -0300

    Bump version: 1.9.0 → 1.9.1

--- setup.py
+++ setup.py
@@ -93,6 +93,6 @@ setup(
     url="https://github.com/inorbit-ai/edge-sdk-python",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="1.9.0",
+    version="1.9.1",
     zip_safe=False,
 )
