0adc8b5e885048d53006b7f8558d86f56a3bae34
Leandro Pineda <leandro@inorbit.ai>
2024-01-04 12:44:22 -0300

    Bump version: 1.11.1 → 1.12.0

--- setup.py
+++ setup.py
@@ -93,6 +93,6 @@ setup(
     url="https://github.com/inorbit-ai/edge-sdk-python",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="1.11.1",
+    version="1.12.0",
     zip_safe=False,
 )
