7b86c81cc1bca852c6b403305bedebd322fc62a5
Jehan Shah <55636546+jehanshah8@users.noreply.github.com>
2025-09-26 14:10:08 -0400

    Bump version: 1.23.1 → 1.24.0 (#82)
    Co-authored-by: Jehan Shah <jehan@inorbit.ai>

--- inorbit_edge/__init__.py
+++ inorbit_edge/__init__.py
@@ -6,7 +6,7 @@ __author__ = "InOrbit"
 __email__ = "support@inorbit.ai"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "1.23.1"
+__version__ = "1.24.0"
 
 
 def get_module_version():
